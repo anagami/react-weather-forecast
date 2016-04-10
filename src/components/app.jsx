bd47d5e449fd71b36f7633259d7f9bf63855c8e8
Your Name <you@example.com>
2016-04-10 13:00:19 +0300

    add bootstrap

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import 'bootstrap'
+
 export default class App extends Component {
     render() {
         return <b>!111!!!444444!!!!!!333!!33333!!!</b>
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+var webpack = require('webpack');
 var WebpackNotifierPlugin = require('webpack-notifier');
+var ExtractTextPlugin = require ('extract-text-webpack-plugin');
+var BowerWebpackPlugin = require('bower-webpack-plugin')
 var path = require('path');
 
 module.exports = {
@@ -9,7 +12,8 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: ['', '.js', '.jsx'],
+        modulesDirectories: ["./src", "node_modules", "bower_components"]
     },
     module: {
         preLoaders: [
@@ -23,12 +27,29 @@ module.exports = {
                 test: /\.jsx?$/,
                 loaders: ['react-hot', 'babel-loader'],
                 exclude: /(node_modules|bower_components)/,
-                plugins: ['transform-runtime'],
+                plugins: ['transform-runtime']
             },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+            {
+                test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+                loader: 'file?name=[path][name].[ext]'
+            }
         ]
     },
     plugins: [
-        new WebpackNotifierPlugin()
+        new WebpackNotifierPlugin(),
+        new BowerWebpackPlugin({
+            excludes: /.*\.less/,
+            searchResolveModulesDirectories: false
+        }),
+        new webpack.ProvidePlugin({
+          $:      "jquery",
+          jQuery: "jquery"
+        }),
+        new ExtractTextPlugin('bundle.css', {allChunks: 1})
     ],
     devServer: {
         port: 5555
